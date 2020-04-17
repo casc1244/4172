e2b23806ed14633dd3f914c1d471fcf87413a01f
casc1244 <1356602646@qq.com>
2020-04-17 15:26:22 +0800

    1472.cpp

--- 4172/4172.cpp
+++ 4172/4172.cpp
@@ -1,20 +1,44 @@
-﻿// 4172.cpp : 此文件包含 "main" 函数。程序执行将在此处开始并结束。
-//
+﻿#include <opencv.hpp>
+#include <math.h>
 
-#include <iostream>
+using namespace cv;
+using namespace std;
 
 int main()
 {
-    std::cout << "Hello World!\n";
+	Mat srcMat = imread("D:\\360downloads\\13.png", 0);
+	Mat dspMat = imread("D:\\360downloads\\13.png");
+	Mat binaryMat;
+
+	Mat inversedMat = 255 - srcMat;
+	threshold(inversedMat, binaryMat, 150, 255, THRESH_OTSU);
+
+	Mat element = getStructuringElement(MORPH_RECT, Size(13, 13));
+	morphologyEx(binaryMat, binaryMat, MORPH_OPEN, element);
+
+	vector<vector<Point>> contours;
+	findContours(binaryMat, contours, RETR_EXTERNAL, CHAIN_APPROX_NONE);
+
+	float rate[100];
+	for (int i = 0; i < contours.size(); i++)
+	{
+		RotatedRect rect = minAreaRect(contours[i]);
+		Point2f vtx[4];
+		rect.points(vtx);
+
+		float Y = sqrt((vtx[0].y - vtx[1].y) * (vtx[0].y - vtx[1].y) + (vtx[0].x - vtx[1].x) * (vtx[0].x - vtx[1].x));
+		float X = sqrt((vtx[1].y - vtx[2].y) * (vtx[1].y - vtx[2].y) + (vtx[1].x - vtx[2].x) * (vtx[1].x - vtx[2].x));
+		rate[i] = X / Y;
+
+		if ((rate[i] >= 0.95) and (rate[i] <= 1.1))
+		{
+			for (int j = 0; j <= 3; j++)
+			{
+				line(dspMat, vtx[j], vtx[(j + 1) % 4], Scalar(0, 255, 0), 1);
+			}
+		}
+	}
+	imshow("dspMat", dspMat);
+	waitKey(0);
+	return 0;
 }
-
-// 运行程序: Ctrl + F5 或调试 >“开始执行(不调试)”菜单
-// 调试程序: F5 或调试 >“开始调试”菜单
-
-// 入门使用技巧: 
-//   1. 使用解决方案资源管理器窗口添加/管理文件
-//   2. 使用团队资源管理器窗口连接到源代码管理
-//   3. 使用输出窗口查看生成输出和其他消息
-//   4. 使用错误列表窗口查看错误
-//   5. 转到“项目”>“添加新项”以创建新的代码文件，或转到“项目”>“添加现有项”以将现有代码文件添加到项目
-//   6. 将来，若要再次打开此项目，请转到“文件”>“打开”>“项目”并选择 .sln 文件
